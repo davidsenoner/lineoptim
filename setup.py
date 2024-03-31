28b0d54627bde94c329675026a68f704eeca064e
David <david.senoner@gmail.com>
2024-03-31 18:17:26 +0100

    feat: fetch readme file in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
+def fetch_readme() -> str:
+  """
+  This function reads the README.md file in the current directory.
+
+  Returns:
+      The lines in the README file.
+  """
+  with open("README.md", encoding="utf-8") as f:
+    return f.read()
+
 setup(
   name='lineoptim',
   version='1.0.0',
   description='A line optimization package for electrical conductors.',
   author='David Senoner',
   author_email='david.senoner@gmail.com',
-  long_description_file='README.md',
-  long_description_content_type='text/markdown',
+  long_description=fetch_readme(),
+  long_description_content_type="text/markdown",
   packages=find_packages(),
   classifiers=[
     'Development Status :: 3 - Alpha',
