ccd2e03e7b8169d8b15210d3019238e8fb8d651c
David Senoner <david.senoner@microtec.eu>
2024-04-11 06:57:54 +0200

    fefat: add static methods to __init__

--- lineoptim/components/__init__.py
+++ lineoptim/components/__init__.py
@@ -1,2 +1,2 @@
-from lineoptim.components.line import Line
+from lineoptim.components.line import Line, compute_partial_voltages, get_dUx, get_current
 from lineoptim.components.network import Network
