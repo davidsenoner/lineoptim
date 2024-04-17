644c0ef30c6ea70014dbfa3e3bff39e14c9efcc9
David Senoner <david.senoner@microtec.eu>
2024-04-17 07:19:33 +0200

    fix: import List from typing

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
+from typing import List
+
 
 def fetch_readme() -> str:
-  """
+    """
   This function reads the README.md file in the current directory.
 
   Returns:
       The lines in the README file.
   """
-  with open("README.md", encoding="utf-8") as f:
-    return f.read()
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
 
 def fetch_requirements(path) -> List[str]:
-  """
+    """
   This function reads the requirements file.
 
   Args:
@@ -20,29 +23,30 @@ def fetch_requirements(path) -> List[str]:
   Returns:
       The lines in the requirements file.
   """
-  with open(path, "r") as fd:
-    return [r.strip() for r in fd.readlines()]
+    with open(path, "r") as fd:
+        return [r.strip() for r in fd.readlines()]
+
 
 setup(
-  name='lineoptim',
-  version='1.1.0',
-  description='A line optimization package for electrical conductors.',
-  author='David Senoner',
-  author_email='david.senoner@gmail.com',
-  long_description=fetch_readme(),
-  long_description_content_type="text/markdown",
-  packages=find_packages(),
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-  ],
-  install_requires=fetch_requirements("requirements.txt"),
-  project_urls={
+    name='lineoptim',
+    version='1.1.0',
+    description='A line optimization package for electrical conductors.',
+    author='David Senoner',
+    author_email='david.senoner@gmail.com',
+    long_description=fetch_readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=fetch_requirements("requirements.txt"),
+    project_urls={
         "Github": "https://github.com/davidsenoner/lineoptim",
     },
 )
