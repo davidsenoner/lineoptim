c469a5c00d3fd643b2dea809b2f16e1b1986d355
David Senoner <david.senoner@microtec.eu>
2024-05-07 06:58:09 +0200

    refactor: use build in method Line.recompute() instead of static method compute_partial_voltages()

--- lineoptim/components/network.py
+++ lineoptim/components/network.py
@@ -36,7 +36,7 @@ class NetworkOptimizer(nn.Module):
     def forward(self):
         self.line.set_resistivity_tensor(self.resistivity)  # update resistivity
 
-        compute_partial_voltages(self.line, iterations=2)
+        self.line.recompute()  # recompute partial voltages
 
         dux = self.line.get_residual_voltage_tensor()  # compute residual voltage
 
